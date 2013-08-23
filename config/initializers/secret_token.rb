f297fd42524bc4a85d80470d701aa61868f89611
Derek Chen <derekchen14@gmail.com>
2013-08-23 08:01:53 -0700

    Make first commit with updated gitignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notable::Application.config.secret_token = '98c89c65d60cbf215056104a4e00711cbd67dc930f5eaced5ff8042da75c567f873556506be31d6d3567eab263b936f6dcb60c6b8b5b5c1e837cec6c2c0c25ab'
