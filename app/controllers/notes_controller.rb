21a349d9f15dd4d369a7f8e9fefea74d398291fe
Derek Chen <derekchen14@gmail.com>
2013-09-13 22:13:32 -0700

    Remove root object from active model serializer

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,49 +1,34 @@
 class NotesController < ApplicationController
-  # GET /notes
+  respond_to :html, :json
+
   # GET /notes.json
   def index
-    @notes = Note.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @notes }
-    end
+    respond_with(@notes = Note.all)
   end
 
-  # GET /notes/1
   # GET /notes/1.json
   def show
     @note = Note.find(params[:id])
-
-    # respond_with
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @note }
-    end
+    respond_with @note
   end
 
-  # GET /notes/new
   # GET /notes/new.json
   def new
     @note = Note.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @note }
-    end
+    respond_with @note
   end
 
   # GET /notes/1/edit
   def edit
     @note = Note.find(params[:id])
+    respond_with @note
   end
 
-  # POST /notes
   # POST /notes.json
   def create
     @note = Note.new(params[:note])
 
-    respond_to do |format|
+    respond_with(@note) do |format|
       if @note.save
         format.html { redirect_to @note, notice: 'Note was successfully created.' }
         format.json { render json: @note, status: :created, location: @note }
@@ -54,12 +39,11 @@ class NotesController < ApplicationController
     end
   end
 
-  # PUT /notes/1
   # PUT /notes/1.json
   def update
     @note = Note.find(params[:id])
 
-    respond_to do |format|
+    respond_with(@note) do |format|
       if @note.update_attributes(params[:note])
         format.html { redirect_to @note, notice: 'Note was successfully updated.' }
         format.json { head :no_content }
@@ -70,13 +54,11 @@ class NotesController < ApplicationController
     end
   end
 
-  # DELETE /notes/1
   # DELETE /notes/1.json
   def destroy
     @note = Note.find(params[:id])
     @note.destroy
-
-    respond_to do |format|
+    respond_with(@note) do |format|
       format.html { redirect_to notes_url }
       format.json { head :no_content }
     end
