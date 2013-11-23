fec8f87ca646bc353320f7112bf32cb6b9e5e4e4
Derek Chen <derekchen14@gmail.com>
2013-11-22 18:36:23 -0800

    Ticket0314: Connect Notable through to the Evernote API

--- app/controllers/evernote_controller.rb
+++ app/controllers/evernote_controller.rb
@@ -2,15 +2,19 @@ class EvernoteController < ApplicationController
 	OAUTH_CONSUMER_KEY = "derekchen14"
 	OAUTH_CONSUMER_SECRET = "c9832e0db4cdb848"
 	SANDBOX = true
+	AUTH_TOKEN = 'S=s1:U=678f0:E=145b5f10d26:C=13e5e3fe128:P=1cd:A=en-devtoken:V=2:H=0c4f9a7b8ed1c264477d9608572f4d0d'
 
 	def start
 		@foovariable = "chocolate"
 	end
 
 	def end
-		@client ||= EvernoteOAuth::Client.new(token: 'S=s1:U=678f0:E=145b5f10d26:C=13e5e3fe128:P=1cd:A=en-devtoken:V=2:H=0c4f9a7b8ed1c264477d9608572f4d0d')
-		@user = en_user
-		@barvariable = "strawberry"
+		@client ||= EvernoteOAuth::Client.new(token: AUTH_TOKEN)
+
+		@user = evernote_user
+		@notebooks = evernote_notebooks
+		@note_count = total_note_count
+		@notebook_count = @notebooks.length
 	end
 
 	private
@@ -22,18 +26,18 @@ class EvernoteController < ApplicationController
 			@note_store ||= @client.note_store
 		end
 
-		def en_user
+		def evernote_user
 			user_store.getUser
 		end
 
-		def notebooks
+		def evernote_notebooks
 			@notebooks ||= note_store.listNotebooks
 		end
 
 		def total_note_count
 			filter = Evernote::EDAM::NoteStore::NoteFilter.new
-			counts = note_store.findNoteCounts(auth_token, filter, false)
-			notebooks.inject(0) do |total_count, notebook|
+			counts = note_store.findNoteCounts(AUTH_TOKEN, filter, false)
+			evernote_notebooks.inject(0) do |total_count, notebook|
 				total_count + (counts.notebookCounts[notebook.guid] || 0)
 			end
 		end
