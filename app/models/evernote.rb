dc8ef3a455afdf9978d2889cf0396fed2b823b75
Derek Chen <derekchen14@gmail.com>
2013-12-07 15:38:36 -0800

    Temporarily remove ActiveModel code from Evernote model to allow for push to Heroku

--- app/models/evernote.rb
+++ app/models/evernote.rb
@@ -1,8 +1,8 @@
 #Evernote model directly passes payload data to Evernote, so we can
 #just use a tableless model.  This means we can include ActiveModel,
 #rather than inheriting from ActiveRecord::Base
-class Evernote
-	include ActiveModel::Model
+class Evernote < ActiveRecord::Base
+  # include ActiveAttr::Model
 	attr_accessor :forest, :trunk, :root, :lastSyncTime, :lastUpdateCount
 
 end
