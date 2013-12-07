980aa08fa26f0eae64e2235ed707079b9fed3de6
Derek Chen <derekchen14@gmail.com>
2013-12-07 15:44:44 -0800

    Temporarily remove Evernote Model completely to test Heroku - fifth attempt

--- app/models/evernote.rb
+++
@@ -1,9 +0,0 @@
-#Evernote model directly passes payload data to Evernote, so we can
-#just use a tableless model.  This means we can include ActiveModel,
-#rather than inheriting from ActiveRecord::Base
-class Evernote < ActiveRecord::Base
-  # include ActiveAttr::Model
-	attr_accessor :forest, :trunk, :root, :lastSyncTime, :lastUpdateCount
-
-end
-
