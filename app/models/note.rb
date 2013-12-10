804f3599a5e30bbb8cef38857c6c162ddf02ee40
Mikechaos <mikechaos13@gmail.com>
2013-12-10 05:30:47 +0000

    Get a list of a branch and all it's descendant, Ticket0496

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,11 +2,45 @@ class Note < ActiveRecord::Base
 	attr_accessible :guid, :title, :subtitle, :parent_id, :rank, :depth, :collapsed
 	validates_presence_of :guid, :rank, :depth
 
-	def compileRoot (branches)
+	def self.compileRoot
+		compiledRoots = []
+		roots = Note.where("parent_id ='root'")
+		roots.each do |root|
+			compiledRoots.push Note.getCompleteDescendantList root
+		end
+		compiledRoots.each do |r|
+			puts "ROOT --"
+			r.each do |branch|
+				puts branch.title
+			end
+		end
 		# collect underpants
 		# some kind of loop function ?
 		# profit!!!
 		# http://knowyourmeme.com/memes/profit
+	end
+	
+	def self.getCompleteDescendantList (root)
+		descendantsList = []
+		rec = -> (current) do
+			descendantsList.push current
+			descendants = Note.getDescendants current
+			descendants.each do |d|
+				rec.call d
+			end
+		end
+		rec.call root
+		descendantsList
+	end
+
+	def self.rootIsFresh (descendantList)
+		descendantList.each do |descendant|
+
+		end
+	end
+	
+	def self.getDescendants (branch)
+		Note.where "parent_id = '#{branch.guid}'"
 	end
 
 	def compileBranches (root)
@@ -17,11 +51,11 @@ class Note < ActiveRecord::Base
 		fresh = false
 		guid = @noteGuid
 	end
-
+	
 	def markStale
 		if branch.isCompiled?
 			branch.fresh = false
 		end
 	end
-
+	
 end
