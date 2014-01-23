cde2bdfe223bc86fad6a3c137b894ef5460552f6
Mikechaos <mikechaos13@gmail.com>
2014-01-23 18:34:12 +0000

    Migration added eng column to Notebooks, Ticket0661

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140122215344) do
+ActiveRecord::Schema.define(:version => 20140123183228) do
 
   create_table "notebooks", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20140122215344) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "guid"
+    t.string   "eng"
   end
 
   create_table "notes", :force => true do |t|
