0509d8d8274194966d82cad9790feab88e336edf
Mikechaos <mikechaos13@gmail.com>
2014-01-23 19:48:51 +0000

    Notebooks are now sent to Evernote on Sync, Ticket0661 But there are no notes in the notebooks in evernote.. yet

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,5 +1,5 @@
 class Notebook < ActiveRecord::Base
-  attr_accessible :guid, :title, :modview, :user_id
+  attr_accessible :guid, :title, :modview, :user_id, :eng
   has_many :notes, dependent: :destroy
   belongs_to :user
 
