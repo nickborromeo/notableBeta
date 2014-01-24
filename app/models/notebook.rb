17c8758ef0a7eb0ced814fd8b57d2d7e6d1eda36
Mikechaos <mikechaos13@gmail.com>
2014-01-24 07:07:40 +0000

    Notebooks are now trashed instead of destroyed, Ticket0661

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,5 +1,5 @@
 class Notebook < ActiveRecord::Base
-  attr_accessible :guid, :title, :modview, :user_id, :eng
+  attr_accessible :guid, :title, :modview, :user_id, :eng, :trashed
   has_many :notes, dependent: :destroy
   belongs_to :user
 
