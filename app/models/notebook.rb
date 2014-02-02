ec4e49ec664131236b9542abaa2a06a79b3548be
Mikechaos <mikechaos13@gmail.com>
2014-02-02 01:35:51 +0000

    Create the notebooks received from Backbone, Ticket0673

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -13,4 +13,16 @@ class Notebook < ActiveRecord::Base
 		notebook.destroy
 	end
 
+	# listOfNotebooks => array received from backbone
+	# [{0: {name: "[NAME]", eng: "[EVERNOTE_GUID]"}, {1: ...
+	def self.createNotebooks (listOfNotebooks, connected_user) 
+		listOfNotebooks.each do |key, n| # n => notebook
+			if Notebook.where("eng='#{n[:eng]}'").empty?
+				fields = {guid: n[:eng], eng: n[:eng], title: n[:name], modview: "outline", user_id: connected_user.id}
+				notebook = Notebook.new fields
+				notebook.save
+			end
+		end
+	end
+
 end
