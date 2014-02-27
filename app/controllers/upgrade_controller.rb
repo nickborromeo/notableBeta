5b39d377837b9cebbfe9fa9cfcdca564379e48db
Derek Chen <derekchen14@gmail.com>
2014-02-26 18:51:52 -0800

    Remove extra users to save on database size

--- app/controllers/upgrade_controller.rb
+++ app/controllers/upgrade_controller.rb
@@ -9,4 +9,16 @@ class UpgradeController < ApplicationController
   def delivery
   end
 
+  def danger
+    @users = User.order("email")
+  end
+
+  def ultra
+  	puts "ultra----------------------"
+  	blanket = params[:id]
+  	User.find(params[:id]).destroy
+    redirect_to custom_thing_url
+    flash[:success] = "User #{blanket} successfully removed."
+  end
+
 end
